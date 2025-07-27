926507381ba16b301f445c85c7d45b1ea75738ad
Gardas Akash <akash39g@gmail.com>
2025-07-27 12:09:05 +0530

    Form data retrival and clear - updated

--- src/index-script.js
+++ src/index-script.js
@@ -6,24 +6,19 @@
  */
 
 const formEle = document.getElementById("form");
-const startTimeEle = document.getElementById("start-time");
-const endTimeEle = document.getElementById("end-time");
-const dayEle = document.getElementById("day");
-const monthEle = document.getElementById("month");
-const yearEle = document.getElementById("year");
-const activityEle = document.getElementById("activity-name");
-const addActivityButton = document.getElementById("add-activity");
 
-formEle.addEventListener("submit", (event) => {
+formEle.addEventListener("submit", function(event) {
   event.preventDefault(); // This is crucial! Stops page reload
 
+  const formElements = this.elements;
   // form data can be retrived now.
-  const startTime = startTimeEle.value;
-  const endTime = endTimeEle.value;
-  const day = dayEle.value;
-  const month = monthEle.value;
-  const year = yearEle.value;
-  const activityName = activityEle.value;
+  const startTime = formElements.startTime.value;
+  const endTime = formElements.endTime.value;
+  const day = formElements.day.value;
+  const month = formElements.month.value;
+  const year = formElements.year.value;
+  const activityName = formElements.activityName.value;
+
+  formEle.reset();
   console.log(startTime, endTime, day, month, year, activityName);
-  console.log("Hello World");
 });
